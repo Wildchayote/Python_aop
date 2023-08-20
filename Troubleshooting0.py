c11a38e26c154c7fd2aedff4e50a32b4a3c063d9
Bashir Sanni <bashirsanni4@gmail.com>
2023-08-20 20:08:27 +0100

    Update Troubleshooting0.py
    Troubleshooting this small block to see if it works before applying to the main block.

---
+++ Troubleshooting0.py
@@ -0,0 +1,27 @@
+prod = {'CA11':'Aisle AA-07',  'CA22':'Aisle AA-11',
+                    'FO11':'Aisle AA-01',   'FO22':'Aisle AA-06',
+                    'BI11':'Aisle AA-15',   'BI22':'Aisle AB-32',
+                    'CO11':'Aisle AA-05',   'CO22':'Aisle AA-02',
+                    'JS11':'Aisle AA-12',   'JS22':'Aisle AA-08', 
+                    'MA11':'Aisle AA-09',   'GS11':'Aisle AA-10',
+                    'SA11':'Aisle AA-13',   'TS11':'Aisle AA-14',
+                    'TB09':'Aisle AA-16',   'PN11':'Aisle AA-04',
+                    
+                    'AB75': 'Aisle BE-79',  'DP30': 'Aisle BA-04',
+                    'SM75': 'Aisle BE-47',  'CE35': 'Aisle BA-90',
+                    'PE7L': 'Aisle BL-50',  'FG1L': 'Aisle BB-86'}
+
+short = ['Aisle AA-16', 'Aisle BE-79','Aisle AA-05', 'Aisle BB-86', 'Aisle BA-90', 'Aisle BA-04', 'Aisle AA-09']
+print(sorted(short))
+
+#short = ['CA11', 'CA22','CO11', 'FO11','SM75', 'CO22', 'FO22', 'BI11', 'BI22','AB75','PE7L']
+long = []
+for i in prod:
+    if prod[i] not in short:
+        continue
+    else:
+        new_list = list(map(lambda s: i, sorted(short)))
+        new_list = (new_list[0])[0:8]
+        #new_list = new_list[0]
+        long.append(new_list)
+print(long)
