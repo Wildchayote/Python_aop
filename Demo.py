65bcf8dde155197759a317e4e696bbed26f7ba71
Bashir Sanni <bashirsanni4@gmail.com>
2023-09-15 10:32:37 +0100

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,9 @@
+user =input('Enter Name: ')
+isFemale =True
+State =input('State: ')
+
+if user =='Adeola' and isFemale==True and State=='Ogun':
+    print('Wife material- Marry her :)')
+else:
+    print('We don\'t know')                                                             
+
