5cf2711ae84dbc73857999b716ad85af73391719
Bashir Sanni <bashirsanni4@gmail.com>
2023-09-15 12:34:14 +0100

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -1,9 +1,15 @@
 user =input('Enter Name: ')
 isFemale =True
-State =input('State: ')
+State =input('State: ')                                                           
+print('Wife material- Marry her :)' if (user == 'Adeola' and isFemale == True and State == 'Ogun') else 'We don\'t know')
 
-if user =='Adeola' and isFemale==True and State=='Ogun':
-    print('Wife material- Marry her :)')
-else:
-    print('We don\'t know')                                                             
+
+# user =input('Enter Name: ')
+# isFemale =True
+# State =input('State: ')
+
+# if user =='Adeola' and isFemale==True and State=='Ogun':
+#     print('Wife material- Marry her :)')
+# else:
+#     print('We don\'t know')                                                             
 
