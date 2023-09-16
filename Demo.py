5a44008bf98a7e13d9b27dae9ebc3cb5c41fbcee
Bashir Sanni <bashirsanni4@gmail.com>
2023-09-16 18:19:29 +0100

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -1,20 +1,5 @@
-user =input('Enter Name: ')
-isFemale =True
-State =input('State: ')                                                           
-print('Wife material- Marry her :)' if (user == 'Adeola' and isFemale == True and State == 'Ogun') else 'Don\'t marry her')
-
-
-
-
-
-
-
-# user =input('Enter Name: ')
-# isFemale =True
-# State =input('State: ')
-
-# if user =='Adeola' and isFemale==True and State=='Ogun':
-#     print('Wife material- Marry her :)')
-# else:
-#     print('We don\'t know')                                                             
+cities = { "New York": 18.65e6, "Cairo": 18.82e6, "Mumbai": 19.32e6, "São Paulo": 20.88e6 , "Lagos": 25.53e6}
+for city in cities:
+    if cities[city]>20e6:
+        print(city, cities[city])                                                         
 
