7c562892820fdb41c29be847ee98d5bdc762b2b9
Bashir Sanni <bashirsanni4@gmail.com>
2023-10-18 18:44:02 +0100

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from World_Atlas.views import country_form, get_capital
-
-urlpatterns = [
-    path('country/', country_form, name = 'country_form'),
-    path('result/<int:pk>/', get_capital, name = 'get_capital')
-]
