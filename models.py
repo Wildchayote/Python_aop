eae1bb186d23c44f847736d388b046ad45397275
Bashir Sanni <bashirsanni4@gmail.com>
2023-10-18 18:45:06 +0100

    Delete models.py

--- models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Nation(models.Model):
-    nation = models.CharField(max_length=120)
-    capital = models.CharField(max_length=120)
-    date = models.DateTimeField(auto_now_add=True)
